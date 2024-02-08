bf4041ec20cd93ca81fed8dd6a81309ca51f5050
Mjoliva027 <150649776+Mjoliva027@users.noreply.github.com>
2024-02-08 14:15:55 +0800

    Add files via upload

---
+++ MyPortfolio/script.js
@@ -0,0 +1,22 @@
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var menu = document.getElementById("sidebar");
+function openmenu(){
+    menu.style.right ="0";
+}
+function closemenu(){
+    menu.style.right="-150px";
+}
+
