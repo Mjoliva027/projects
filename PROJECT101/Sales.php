9fbb85b929d161c56983d31d5dddc6b9be30298a
Mjoliva027 <150649776+Mjoliva027@users.noreply.github.com>
2024-06-01 23:50:31 +0800

    Add files via upload

--- PROJECT101/Sales.php
+++ PROJECT101/Sales.php
@@ -18,7 +18,7 @@ if(isset($_POST['accept_order'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Once Admin Page</title>
+   <title>Admin Page</title>
     <link rel="stylesheet" href="css/adminCSS.css">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
   
